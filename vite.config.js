0ca074628aa2fb91b40340e4cba3cf08aa58242f
Suraj Prajapati <suraj26july003@gmail.com>
2025-11-25 12:11:18 +0530

    Configure base path in Vite config
    Set the base path for the Vite project.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Gemini-2.0/",   // <-- यह लाइन ज़रूरी है (repo का नाम)
 })
